38b3fa045bcdb56ed8314ffe7d703659d0bb4a5a
lobarzanek <lukiob@gmail.com>
2024-04-07 21:59:41 +0200

    Create project struct

---
+++ components/Navbar/index.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+const pages = [
+    {
+        src: "/",
+        name: "Home"
+    },
+    {
+        src: "/artists",
+        name: "Artyści"
+    },
+    {
+        src: "/offer",
+        name: "Oferta"
+    },
+    {
+        src: "/ask",
+        name: "Zapytaj o tatuaż"
+    },
+    {
+        src: "/contact",
+        name: "Kontakt"
+    },
+]
+
+</script>
+<template>
+<div>
+    <NuxtLink v-for="page in pages" to="{{ page.src }}">{{ page.name }}</NuxtLink>
+</div>
+</template>
---
+++ pages/contact.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+  <h1>contact</h1>
+</template>
