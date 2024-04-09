33f463649bdd61193c9936b3e156de774dc53d14
lobarzanek <lukiob@gmail.com>
2024-04-09 22:54:56 +0200

    Changed title and description to reusable components

--- pages/artists.vue
+++ pages/artists.vue
@@ -1,17 +1,17 @@
 <script setup>
 const artists = [
   {
-    name: "Natalia 'InkQueen' Nowak",
+    name: "Natalia 'Queen' Nowak",
     description:
-      "Natalia, znana również jako 'InkQueen', jest naszą mistrzynią detali. Jej niezwykła precyzja i umiejętność wyrażania emocji za pomocą igły sprawiają, że jej prace są prawdziwymi arcydziełami.",
+      "Natalia, znana również jako 'Queen', jest naszą mistrzynią detali. Jej niezwykła precyzja i umiejętność wyrażania emocji za pomocą igły sprawiają, że jej prace są prawdziwymi arcydziełami.",
     imgSrc:
       "https://cdn.pixabay.com/photo/2016/11/29/05/16/woman-1867485_960_720.jpg",
     imgAlt: "Natalia 'InkQueen' Nowak",
   },
   {
-    name: "Adam 'DarkInk' Kowalski",
+    name: "Adam 'Dark' Kowalski",
     description:
-      "Adam, nazywany również 'DarkInk', jest mistrzem w tworzeniu mrocznych i pełnych charakteru dzieł sztuki na ciele. Jego styl to połączenie realistycznych elementów z surrealistycznymi motywami.",
+      "Adam, nazywany również 'Dark', jest mistrzem w tworzeniu mrocznych i pełnych charakteru dzieł sztuki na ciele. Jego styl to połączenie realistycznych elementów z surrealistycznymi motywami.",
     imgSrc:
       "https://cdn.pixabay.com/photo/2016/11/29/05/18/adult-1867489_960_720.jpg",
     imgAlt: "Adam 'DarkInk' Kowalski",
@@ -20,13 +20,13 @@ const artists = [
 </script>
 
 <template>
-  <h1 class="font-bold uppercase text-3xl text-center mb-4">Artyści</h1>
-  <div class="m-4 text-center text-lg lg:mx-56">
+  <PageTitle title="artyści" />
+  <PageDescription>
     Poznaj naszych utalentowanych artystów, którzy sprawią, że Twój pomysł
     stanie się rzeczywistością. Każdy z nich ma swoje unikalne podejście i
     specjalizuje się w różnych stylach, ale razem tworzymy harmonijną pracownię,
     gdzie każdy klient czuje się wyjątkowo.
-  </div>
+  </PageDescription>
   <div class="flex justify-center gap-8 flex-wrap m-4 lg:gap-24">
     <ArtistsCard
       v-for="a in artists"
