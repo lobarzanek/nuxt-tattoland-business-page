e66ad8e9505e747e7030998ed499832f58e19544
lobarzanek <lukiob@gmail.com>
2024-04-09 22:53:46 +0200

    Added home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,26 @@
 <script setup></script>
 
 <template>
-  <h1>home</h1>
+  <div class="flex justify-center flex-col items-center gap-16">
+    <div class="mt-8 md:mt-12 lg:mt-36">
+      <p class="font-bold text-2xl text-center lg:text-5xl">
+        Witaj w Tattoo<span class="text-flamingo">Land</span>!
+      </p>
+    </div>
+    <div>Twoje miejsce, gdzie sztuka spotyka ciało.</div>
+    <div class="flex flex-wrap gap-8 justify-center">
+      <button
+        @click="navigateTo({ path: '/offer' })"
+        class="btn btn-outline w-5/6"
+      >
+        Poznaj naszą ofertę
+      </button>
+      <button
+        @click="navigateTo({ path: '/artists' })"
+        class="btn btn-outline w-5/6"
+      >
+        Poznaj naszych artystów
+      </button>
+    </div>
+  </div>
 </template>
