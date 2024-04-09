af94995702c25f591320e5a9149e05038dddff69
lobarzanek <lukiob@gmail.com>
2024-04-09 19:38:21 +0200

    Create title component

---
+++ components/Page/Title.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: () => "",
+  }
+});
+</script>
+
+<template>
+  <h1 class="font-bold uppercase text-3xl text-center mb-4">
+    {{ title }}
+  </h1>
+</template>
