bdf93aced5ad0ae6f969aff1d4d419c830d362aa
lobarzanek <lukiob@gmail.com>
2024-04-09 18:52:56 +0200

    Added artists page

---
+++ components/ArtistsCard.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    default: () => "",
+  },
+  description: {
+    type: String,
+    default: () => "",
+  },
+  imgSrc: {
+    type: String,
+    default: () => "",
+  },
+  imgAlt: {
+    type: String,
+    default: () => "",
+  },
+});
+</script>
+
+<template>
+  <div class="max-w-sm rounded-xl overflow-hidden shadow-lg">
+    <img class="w-full" :src="imgSrc" :alt="imgAlt" />
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ name }}</div>
+      <p class="text-black-700 text-base">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
--- pages/artists.vue
+++ pages/artists.vue
@@ -1,5 +1,39 @@
-<script setup></script>
+<script setup>
+const artists = [
+  {
+    name: "Natalia 'InkQueen' Nowak",
+    description:
+      "Natalia, znana również jako 'InkQueen', jest naszą mistrzynią detali. Jej niezwykła precyzja i umiejętność wyrażania emocji za pomocą igły sprawiają, że jej prace są prawdziwymi arcydziełami.",
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2016/11/29/05/16/woman-1867485_960_720.jpg",
+    imgAlt: "Natalia 'InkQueen' Nowak",
+  },
+  {
+    name: "Adam 'DarkInk' Kowalski",
+    description:
+      "Adam, nazywany również 'DarkInk', jest mistrzem w tworzeniu mrocznych i pełnych charakteru dzieł sztuki na ciele. Jego styl to połączenie realistycznych elementów z surrealistycznymi motywami.",
+    imgSrc:
+      "https://cdn.pixabay.com/photo/2016/11/29/05/18/adult-1867489_960_720.jpg",
+    imgAlt: "Adam 'DarkInk' Kowalski",
+  },
+];
+</script>
 
 <template>
-  <h1>artists</h1>
+  <h1 class="font-bold uppercase text-3xl text-center mb-4">Artyści</h1>
+  <div class="m-4 text-center text-lg lg:mx-56">
+    Poznaj naszych utalentowanych artystów, którzy sprawią, że Twój pomysł
+    stanie się rzeczywistością. Każdy z nich ma swoje unikalne podejście i
+    specjalizuje się w różnych stylach, ale razem tworzymy harmonijną pracownię,
+    gdzie każdy klient czuje się wyjątkowo.
+  </div>
+  <div class="flex justify-center gap-8 flex-wrap m-4 lg:gap-24">
+    <ArtistsCard
+      v-for="a in artists"
+      :name="a.name"
+      :description="a.description"
+      :imgSrc="a.imgSrc"
+      :imgAlt="a.imgAlt"
+    />
+  </div>
 </template>
