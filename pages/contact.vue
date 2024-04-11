8528f07a3662cd935efe182efb687d4aea4a388c
lobarzanek <lukiob@gmail.com>
2024-04-11 19:17:17 +0200

    Added contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,18 @@
 <script setup></script>
 
 <template>
-  <h1>contact</h1>
+  <PageTitle title="Kontakt" />
+  <div class="flex justify-center py-4">
+    <div class="flex flex-col items-start gap-5 text-lg  lg:mx-56 lg:my-8">
+      <span class="inline-flex items-center flex-wrap py-2">
+        <SVGAddress class="mr-3" /> ul. Kopytkowa 14, <span> 80-268 Gdańsk</span>
+      </span>
+      <span class="inline-flex items-center py-2">
+        <SVGPhone class="mr-3" /> Tel: 548-659-947
+      </span>
+      <span class="inline-flex items-center py-2">
+        <SVGMail class="mr-3" /> tattooland@tattooland.pl
+      </span>
+    </div>
+  </div>
 </template>
