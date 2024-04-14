06e853848c3720167b5c47c5350b210c4a1feb1d
lobarzanek <lukiob@gmail.com>
2024-04-14 16:41:04 +0200

    Added offer page

---
+++ components/Offer/Carousel.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="carousel lg:w-1/3">
+  <div id="slide1" class="carousel-item relative w-full">
+    <img src="https://cdn.pixabay.com/photo/2015/08/16/12/39/man-890887_960_720.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a> 
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src="https://cdn.pixabay.com/photo/2019/04/12/21/19/woman-4123270_960_720.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a> 
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div> 
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src="https://cdn.pixabay.com/photo/2021/04/28/07/45/tattoo-6213256_960_720.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a> 
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
+</template>
--- pages/offer.vue
+++ pages/offer.vue
@@ -1,5 +1,13 @@
 <script setup></script>
 
 <template>
-  <h1>offer</h1>
+  <PageTitle title="Oferta" />
+  <PageDescription>
+    Oferujemy szeroki zakres usług, w tym tatuaże i piercing. Głównie skupiamy
+    się na stylu tatuażu Old School, który jest naszą pasją i specjalizacją.
+    Poniżej nasze przykładowe realizacje.
+  </PageDescription>
+  <div class="flex justify-center my-16">
+    <OfferCarousel />
+  </div>
 </template>
